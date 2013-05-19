b97c7a6384806b72311f4ef166ea4dc2aef1ae3e
jescacena <javier.escacena@gmail.com>
2013-05-19 21:59:55 +0200

    the beginning

---
+++ opencart_ccb/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://adeideas.com/opencart/');
+define('HTTP_IMAGE', 'http://adeideas.com/opencart/image/');
+define('HTTP_ADMIN', 'http://adeideas.com/opencart/admin/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://adeideas.com/opencart/');
+define('HTTPS_IMAGE', 'http://adeideas.com/opencart/image/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/vhost/adeideas.com/home/html/opencart/catalog/');
+define('DIR_SYSTEM', '/var/www/vhost/adeideas.com/home/html/opencart/system/');
+define('DIR_DATABASE', '/var/www/vhost/adeideas.com/home/html/opencart/system/database/');
+define('DIR_LANGUAGE', '/var/www/vhost/adeideas.com/home/html/opencart/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/vhost/adeideas.com/home/html/opencart/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/vhost/adeideas.com/home/html/opencart/system/config/');
+define('DIR_IMAGE', '/var/www/vhost/adeideas.com/home/html/opencart/image/');
+define('DIR_CACHE', '/var/www/vhost/adeideas.com/home/html/opencart/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/vhost/adeideas.com/home/html/opencart/download/');
+define('DIR_LOGS', '/var/www/vhost/adeideas.com/home/html/opencart/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'qnz991.adeideas.com');
+define('DB_USERNAME', 'qnz991');
+define('DB_PASSWORD', 'RHINO0s');
+define('DB_DATABASE', 'qnz991');
+define('DB_PREFIX', 'opencart_');
+?>
